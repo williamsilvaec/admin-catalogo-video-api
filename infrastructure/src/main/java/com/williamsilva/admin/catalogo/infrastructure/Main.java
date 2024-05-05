d63f53f86390149fb3b89254f1375fedd39db366
William Silva <williamsilvaec@gmail.com>
2024-05-05 11:09:48 -0300

    configurar o Spring Boot no módulo infrastructure

--- infrastructure/src/main/java/com/williamsilva/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/williamsilva/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,15 @@
 package com.williamsilva.admin.catalogo.infrastructure;
 
+import com.williamsilva.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "dev");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
